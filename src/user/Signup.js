1f0e60d9e714addcea09bf4c7750acba7ea448fb
jose-abel <guest1@guest1s-MacBook-Pro.local>
2022-07-06 15:31:53 -0400

    Dockerize the frontend. Add the environment variable for the port of the backend

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Layout from '../core/Layout';
-
+import { API } from "../config";
 
 const Signup = () => (
   <Layout 
     title="Signup"
     description="Signup to Node React E-Commerce App"
   >
-    ...
+    { API }
   </Layout>
 );
 
