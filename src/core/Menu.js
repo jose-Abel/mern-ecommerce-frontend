97e34de4467a0c3d75ef7328af08be0f53a855c4
jose-abel <guest1@guest1s-MacBook-Pro.local>
2022-07-24 20:32:50 -0400

    Create the functionality to save the token into the localstorage and signout

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { signout, isAuthenticated } from '../auth';
 
 const isActive = (history, path) => {
   if (history.location.pathname === path) {
@@ -24,23 +25,41 @@ const Menu = ({ history }) => {
           </Link>
         </li>
 
-        <li className='nav-item'>
-          <Link
-            className='nav-link'
-            style={isActive(history, '/signin')}
-            to="/signin">
-              Signin
-          </Link>
-        </li>
+        { !isAuthenticated() && (
+          <Fragment>
+            <li className='nav-item'>
+              <Link
+                className='nav-link'
+                style={isActive(history, '/signin')}
+                to="/signin">
+                  Signin
+              </Link>
+            </li>
 
-        <li className='nav-item'>
-          <Link
-            className='nav-link'
-            style={isActive(history, '/signup')}
-            to="/signup">
-              Signup
-          </Link>
-        </li>
+            <li className='nav-item'>
+              <Link
+                className='nav-link'
+                style={isActive(history, '/signup')}
+                to="/signup">
+                  Signup
+              </Link>
+            </li>
+          </Fragment>
+        )}
+
+        { isAuthenticated && (
+          <li className='nav-item'>
+            <span
+              className='nav-link'
+              style={{cursor: 'pointer', color: '#ffffff'}}
+              onClick={() => signout(() => {
+                history.push('/');
+              })}
+            >
+                Signout
+            </span>
+          </li>
+        )}
 
       </ul> 
     </div>
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
-import { signin } from '../auth';
+import { signin, authenticate } from '../auth';
 
 const Signin = () => {
   const [values, setValues] = useState({
@@ -32,10 +32,14 @@ const Signin = () => {
       if (data.error) {
         setValues({...values, error: data.error, loading: false});
       } else {
-        setValues({
-          ...values,
-          redirectToReferrer: true
-        });
+        authenticate(data, 
+          () => {
+            setValues({
+              ...values,
+              redirectToReferrer: true
+            });
+          }
+        )
       }
     })
   }
